fa9891f2239078feeb535717ab9b206e10b78f63
Caroline Liu <caroline.liu111@gmail.com>
2019-09-06 11:07:41 -0400

    deleting base react-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import madrid from '../madrid.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= {madrid} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hi, my name is Caroline Liu. I’m a second-year Computer Science and Design Major at Northeastern University. My interest in computer science began when I had to create a website during my freshman year of high school. I was so enthusiastic about this project that I spent many hours researching web design and user interface design on my own time. I continued to pursue my interest by taking AP Computer Science and now, college courses to learn how to design programs and how to design with the user in mind. Most recently, I have continued my learning outside of the classroom by attending tech talks and exploring HTML, CSS, and JavaScript through hackathon projects.
         </p>
         <a
           className="App-link"
