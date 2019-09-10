e8ac0f43926b1baf1972414643c544ae0af59b0d
Caroline Liu <caroline.liu111@gmail.com>
2019-09-10 10:26:24 -0400

    added header and react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import madrid from '../madrid.jpg';
+import madrid from './assets/madrid.jpg';
+import hi from './assets/madrid.jpg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src= {madrid} />
-        <p>
-        Hi, my name is Caroline Liu. I’m a second-year Computer Science and Design Major at Northeastern University. My interest in computer science began when I had to create a website during my freshman year of high school. I was so enthusiastic about this project that I spent many hours researching web design and user interface design on my own time. I continued to pursue my interest by taking AP Computer Science and now, college courses to learn how to design programs and how to design with the user in mind. Most recently, I have continued my learning outside of the classroom by attending tech talks and exploring HTML, CSS, and JavaScript through hackathon projects.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="main-content">
+            <header className="header">
+                <div class="header">
+                    <a href="#default" class="logo">CompanyLogo</a>
+                    <div class="header-right">
+                        <a class="active" href="#home">Home</a>
+                        <a href="#contact">Contact</a>
+                        <a href="#about">About</a>
+                    </div>
+                </div>
+            </header>
+            <div className ="body">
+                <img src= {hi} />
+                <p>
+                    Hi, my name is Caroline Liu. I’m a second-year Computer Science and Design Major at Northeastern University. My interest in computer science began when I had to create a website during my freshman year of high school. I was so enthusiastic about this project that I spent many hours researching web design and user interface design on my own time. I continued to pursue my interest by taking AP Computer Science and now, college courses to learn how to design programs and how to design with the user in mind. Most recently, I have continued my learning outside of the classroom by attending tech talks and exploring HTML, CSS, and JavaScript through hackathon projects.
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
